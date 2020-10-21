248aedaba7fe220e265591d118c8743281a5ce60
Jordan Panasewicz <jbpanasewicz@gmail.com>
2020-10-21 21:53:51 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,9 +3,9 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   
   data.each_with_object ({}) do | (key, value), accum |
-    
     value.each do |inner_key, names|
       names.each do |name|
+        binding.pry
         if !accum[name]
           accum[name] = {}
         end
